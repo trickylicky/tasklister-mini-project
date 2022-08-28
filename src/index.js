6d253e5aeacf877a18b00d8c6d99ff8fb525dd12
Tricky Licky <wangombevictor5@gmail.com>
2022-08-28 23:00:05 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.addEventListener("click",(e)=>{
+  const values = document.getElementById("new-task-description").value;
+  const display = document.querySelector("#tasks")
+  display.innerHTML = values;
+  e.preventDefault();
+  
+})
